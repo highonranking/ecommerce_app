490938657697c9e3d6fe40ac36485a5a34ccf709
abhinav dixit <abhinavdixit2306@gmail.com>
2021-08-11 20:07:47 +0530

    Basic setup of Login, Register, Home Routes

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Login from './pages/auth/Login.js';
+import Register from './pages/auth/Register.js';
+import Home from './pages/Home.js';
+
+ 
+const App = ()  => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+
+
+    </Switch>
+  );
+}
+
+export default App;
