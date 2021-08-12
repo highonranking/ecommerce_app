264b996ecf06a0cdd47be4b61973cde9b3cff479
abhinav dixit <abhinavdixit2306@gmail.com>
2021-08-12 23:58:24 +0530

    login and register nav

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,23 +1,32 @@
 import React, {useState} from 'react';
 import { Menu } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import {SettingOutlined, UserOutlined, UserAddOutlined, HomeOutlined } from '@ant-design/icons';
 
 const { SubMenu } = Menu; 
 
 const Header = () => {
-const [current, setCurrent] = useState('')
+const [current, setCurrent] = useState("home")
 
-const handleClick = () => {
-    //
+const handleClick = (e) => {
+    setCurrent(e.key);
 };
 
 return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-        <Menu.Item key="mail" icon={<MailOutlined />}>
+        <Menu.Item key="home" icon={<HomeOutlined /> }>
           Home
         </Menu.Item>
+
+        <Menu.Item key="register" icon={<UserAddOutlined /> } className="float-right">
+          Register
+        </Menu.Item>
        
-        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Register">
+        <Menu.Item key="login" icon={ <UserOutlined/> } className="float-right">
+          Login
+        </Menu.Item>
+
+       
+        <SubMenu icon={<SettingOutlined />} title="Username">
           
             <Menu.Item key="setting:1">Option 1</Menu.Item>
             <Menu.Item key="setting:2">Option 2</Menu.Item>
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//import '~mdb-ui-kit/css/mdb.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import 'antd/dist/antd.css';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
