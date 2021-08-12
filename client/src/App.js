97c15b064626ae00c62bc186006e38b33df279ff
abhinav dixit <abhinavdixit2306@gmail.com>
2021-08-12 23:20:32 +0530

    Navigation, Bootstrap Material, antd setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,21 @@ import {Switch, Route} from 'react-router-dom';
 import Login from './pages/auth/Login.js';
 import Register from './pages/auth/Register.js';
 import Home from './pages/Home.js';
-
+import Header from './components/nav/Header';
  
 const App = ()  => {
   return (
+   <>
+    <Header />
     <Switch>
+
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
 
 
     </Switch>
+   </>
   );
 }
 
