48e5463e8297043c9c3e766245584cae399b373b
abhinav dixit <abhinavdixit2306@gmail.com>
2021-08-14 23:02:11 +0530

    signin email confirmation

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  //firebase.analytics();
 
   // export 
   export const auth = firebase.auth();
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,9 +1,26 @@
 import React, {useState} from 'react';
+import {auth} from '../../firebase';
+import {toast, ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 const Register = () => {
     const [email, setEmail] = useState("");
-    const handleSubmit = () => {
-        //
+    const handleSubmit = async (e) => {
+        e.preventDefault() ;   // prevent browser from reload
+        const config = {
+            url: 'http://localhost:3000/register/complete',
+            handleCodeInApp: true
+        }
+
+        await auth.sendSignInLinkToEmail(email, config);
+        toast.success(`Email is sent to ${email}. Click on the click to complete your Registration.`);
+
+        // local
+        window.localStorage.setItem('emailForRegistration', email);
+
+        //clear
+        setEmail("");
+
     }
     const registerForm = () => <form onSubmit={handleSubmit}>
         <input type="email"
@@ -12,17 +29,20 @@ const Register = () => {
          onChange={e => setEmail(e.target.value)}
          autoFocus
          />
-
-         <button type="submit" className="btn btn-raised">
+        <div className="text-center">
+        <button type="submit" className="btn btn-lg btn-success btn-raised mt-4">
              Register
          </button>
+        </div>
+        
         
     </form>
     return (
         <div className="container p-5">
             <div className="row">
-                <div className="clo-md-6 offset-md-3">
+                <div className="clo-md-6">
                     <h4>Register</h4>
+                    <ToastContainer/>
                     {registerForm()}
                 </div>
             </div>
