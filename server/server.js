ca59fcd0c549501d2e0dfba08ca80ed88f71fd7f
abhinav dixit <abhinavdixit2306@gmail.com>
2021-08-21 00:58:07 +0530

    server setup

---
+++ server/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config()
+
+//app
+const app = express();
+
+//db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+})
+.then(() => console.log("DB CONNECTED"))
+.catch(err => console.log('DB CONNECTION ERR', err ));
+
+//middlewares
+
+app.use(morgan("dev"));
+app.use(express.json({limit: "2mb"}));
+app.use(cors());
+
+//route
+
+app.get('/api', (req, res) => {
+    res.json({
+        data: 'a node api is hitted',
+    });
+});
+
+// port
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server is running on ${port}`));
+
+
+
+
+
+
+
+
+
+
+
