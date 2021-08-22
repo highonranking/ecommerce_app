1573f87eb20e2dc299a5760346a5b2687b436916
abhinav dixit <abhinavdixit2306@gmail.com>
2021-08-23 01:28:22 +0530

    Creating middlewares

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -6,3 +6,5 @@ var serviceAccount = require("../config/fbServiceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
+module.exports = admin;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,15 @@
 const express = require('express');
 
 const router = express.Router();
-const { createOrUpdateUser } = require('../controllers/auth');
 
-router.get('/create-or-update-user', createOrUpdateUser);
+// middlewares
+const {authCheck} = require('../middlewares/auth');
+
+// controllers
+
+const { createOrUpdateUser } = require('../controllers/auth');
+const { auth } = require('firebase-admin');
+
+router.post('/create-or-update-user', authCheck,  createOrUpdateUser);
 
 module.exports = router;
