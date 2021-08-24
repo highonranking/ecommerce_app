cf88fdaff684f200287714042b52300878a8e64b
abhinav dixit <abhinavdixit2306@gmail.com>
2021-08-25 01:49:25 +0530

    update password

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,8 @@ import { currentUser } from "./functions/auth";
 
 import History from "./pages/user/History";
 import UserRoute from "./components/routes/UserRoute";
-
+import Password from "./pages/user/Password";
+import Wishlist from "./pages/user/Wishlist";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -61,6 +62,8 @@ const App = () => {
         <Route exact path="/register/complete" component={RegisterComplete} />
         <Route exact path="/forgot/password" component={ForgotPassword} />
         <UserRoute exact path="/user/history" component={History} />
+        <UserRoute exact path="/user/password" component={Password} />
+        <UserRoute exact path="/user/wishlist" component={Wishlist} />
 
       </Switch>
     </>
