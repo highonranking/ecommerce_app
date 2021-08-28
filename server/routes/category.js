6d8caaeda6d5fa69fd16d9cc304f21806d51ddc5
abhinav dixit <abhinavdixit2306@gmail.com>
2021-08-28 21:30:28 +0530

    category list read update delete

--- server/routes/category.js
+++ server/routes/category.js
@@ -13,7 +13,7 @@ const { create, read, update, remove, list } = require('../controllers/category'
 //routes 
 router.post('/category', authCheck, adminCheck, create);
 router.get('/categories', list);
-router.get('/category/:slug', authCheck, adminCheck, read);
+router.get('/category/:slug', read);
 router.put('/category/:slug', authCheck, adminCheck, update);
 router.delete('/category/:slug', authCheck, adminCheck, remove);
 
