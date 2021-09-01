9c21ce4811a62375a619f9bb5cb863a80b2b248b
abhinav dixit <abhinavdixit2306@gmail.com>
2021-09-01 22:42:33 +0530

    admin nav

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import AdminNav from '../../components/nav/AdminNav';
 
 const AdminDashboard = () => {
     return(
-        <>
-            <p>Admin Dashboard</p>
-        </>
+        <div className="container-fluid">
+        <div className="row">
+            <div className="col-md-2">
+                <AdminNav/>
+            </div>
+            <div className="col">
+                Admin Dashboard
+            </div>
+        </div>
+    </div>
     );
 };
 
